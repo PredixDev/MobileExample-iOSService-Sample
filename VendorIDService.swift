ddaffe872f118e12f38ad277b216bf4c35b5d4e3
Andy Johns <andy.johns@ge.com>
2016-02-01 15:37:09 -0800

    Initial commit

---
+++ VendorIDService.swift
@@ -0,0 +1,194 @@
+//
+//  VendorIDService.swift
+//  PredixMobileReferenceApp
+//
+//  Created by Johns, Andy (GE Corporate) on 1/14/16.
+//  Copyright © 2016 GE. All rights reserved.
+//
+
+/* ****************************
+The purpose of this very simple service is to demonstrate the key components of writing a
+Predix Mobile client service.
+
+This service takes no parameters, and returns the iOS Vendor Device Id as a JSON object.
+
+There are unit tests for this class in the PredixMobileReferenceAppTests target under the Services group.
+**************************** */
+
+
+import Foundation
+
+// import the PredixMobile framework, so Swift can find the PredixMobile components we'll need
+import PredixMobileSDK
+
+// services must implement the ServiceProtocol protocol.
+// As this protocol is defined as Obj-C compatible, the implementer must be an Obj-C compatible class.
+@objc class VendorIDService : NSObject, ServiceProtocol
+{
+    /* ****************************
+    ServiceProtocol's properties and methods are all defined as static, and no class implementation of your service is ever created.
+    This is a purposeful architectural decision. Services should be stateless and interaction with them ephemeral. A static
+    object enforces this direction.
+    **************************** */
+    
+    // the serviceIdentifier property defines first path component in the URL of the service.
+    static var serviceIdentifier : String {get { return "vendorid" }}
+    
+    
+    /* ****************************
+    performRequest is the meat of the service. It is where all requests to the service come in.
+    
+    The request parameter will contain all information the caller has provided for the request, this will include the URL,
+    the HTTP Method, and in the case of a POST or PUT, any HTTP body data.
+
+    The nature of services are asynchronous. So this method has no return values, it communicates with its caller through three
+    blocks or closures. These three are the parameters responseReturn, dataReturn, and requestComplete. This follows the general
+    nature of a web-based HTTP interaction.
+    
+    responseReturn -- generally every call to performRequest should call responseReturn once, and only once. The call requires an
+    NSHTTPResponse object, and a default object is provided as the "response" parameter. The response object can be returned directly,
+    or can be used as a container for default values, and a new NSHTTPResponse can be built from it. The default response parameter's
+    status code is 200 (OK), so error conditions will not return the response object unaltered. (See the respondWithErrorStatus methods,
+    and the createResponse method documentation below for helpers in creating other response objects.)
+    
+    dataReturn -- Services that return data, and not just a status code will use the dataReturn parameter to return data. Generally
+    this block will be called once, however it could be called multiple times to return particularly large amounts of data in a
+    chunked fashion. Again, this behavior follows general web-based HTTP interaction. If used, the dataReturn block should be called after
+    the responseReturn block is called, and before the responseComplete block is called.
+    
+    requstComplete -- this block indicates to the caller that the service has completed processing, and the call is complete. The requestComplete
+    block must be called, and it must be called only once per performRequest call. Once the requestComplete block is called, no additional 
+    processing should happen in the service, and no other blocks should be called.
+    **************************** */
+    static func performRequest(request : NSURLRequest, response : NSHTTPURLResponse, responseReturn : responseReturnBlock, dataReturn : dataReturnBlock, requestComplete: requestCompleteBlock)
+    {
+        
+        // First let's examine the request. In this example, we're going to expect only a GET request, and the URL path should only be the serviceIdentifier
+        
+        // we'll use a guard statement here just to verify the request object is valid. The HTTPMethod and URL properties of a NSURLRequest
+        // are optional, and we need to ensure we're dealing with a request that contains them.
+        
+        guard let url = request.URL, path = url.path, method = request.HTTPMethod else
+        {
+            /* ****************************
+             if the request does not contain a URL or a HTTPMethod, then we return a error. We'll also return an error if the URL
+             does not contain a path. In a normal interaction this would never happen, but we need to be defensive and expect anything.
+            
+             we'll use one of the respondWithErrorStatus methods to return an error condition to the caller, in this case,
+             a status code of 400 (Bad Request).
+            
+             Note that the respondWithErrorStatus methods all take the response object, the reponseReturn block and the requestComplete
+             block. This is because the respondWithErrorStatus constructs an appropriate NSHTTPURLResponse object, and calls
+             the reponseReturn and requestComplete blocks for you. Once a respondWithErrorStatus method is called, the performRequest
+             method should not continue processing and should always return.
+            **************************** */
+            self.respondWithErrorStatus(.BadRequest, response, responseReturn, requestComplete)
+            return
+        }
+        
+        /* ****************************
+         Now that we know we have a path and method string, let's examine them for our expected values.
+         For this example we'll return an error if the url has any additional path or querystring parameters.
+         We'll also return an error if the method is not the expected GET HTTP method. The HTTP Status code convention
+         has standard codes to return in these cases, so we'll use those.
+        **************************** */
+        
+        /* ****************************
+         Path in this case should match the serviceIdentifier, or "vendorid". We know the serviceIdentifier is all
+         lower case, so we ensure the path is too before comparing.
+         
+         We use the serviceIdentifier property here rather than the string "vendorid" as a general best practice of
+         avoiding hard-coded strings in multiple places.
+        
+         In addition, we expect the query string to be nil, as no query parameters are expected in this call.
+        
+         In your own services you may want to be more lenient, simply ignoring extra path or parameters.
+        **************************** */
+        if path.lowercaseString != "/\(self.serviceIdentifier)" || url.query != nil
+        {
+            // In this case, if the request URL is anything other than "http://pmapi/vendorid" we're returning a 400 status code.
+            self.respondWithErrorStatus(.BadRequest, response, responseReturn, requestComplete)
+            return
+        }
+        
+        // now that we know our path is what we expect, we'll check the HTTP method. If it's anything other than "GET"
+        // we'll return a standard HTTP status used in that case.
+        
+        if method != "GET"
+        {
+            // According to the HTTP specification, a status code 405 (Method not allowed) must include an Allow header containing a list of valid methods.
+            // this  demonstrates one way to accomplish this.
+            let headers = ["Allow" : "GET"]
+            
+            // This respondWithErrorStatus overload allows additional headers to be passed that will be added to the response.
+            self.respondWithErrorStatus(HTTPStatusCode.MethodNotAllowed, response, responseReturn, requestComplete, headers)
+            return
+        }
+        
+        
+        // Now we know that our path and method were correct, and we've handled error conditions, we get the device's vendor id.
+        if let vendorId = UIDevice.currentDevice().identifierForVendor
+        {
+            // Let's return the vendorId as a JSON object, a dictionary that contains the key "vendorId" and the value of the id itself.
+            // We could create this many ways, but here we'll use the Apple SDK's JSON serialization:
+            
+            let returnDictionary = ["vendorId" :  vendorId.UUIDString]
+            
+            // NSJSONSerialization.dataWithJSONObject can throw, so we'll do this in a do/try/catch statement.
+            do
+            {
+                let jsonData = try NSJSONSerialization.dataWithJSONObject(returnDictionary, options: NSJSONWritingOptions(rawValue: 0))
+                
+                // Now our jsonData object contains a serialized JSON dictionary ready for consumption by the caller.
+                
+                // Our service call is complete, now we call our blocks, in order.
+                
+                // the default response object is always pre-set with a 200 (OK) response code, so can be directly used when there are no problems.
+                responseReturn(response)
+                
+                // we return the JSON object
+                dataReturn(jsonData)
+                
+                // An inform the caller the service call is complete
+                requestComplete()
+                
+                // We don't need this return here, since we don't have any other code below, but in a more complex service call you may.
+                // After requestComplete is called, you should always ensure no other code is executed in the method.
+                return
+            }
+            catch let error
+            {
+                // Log the error
+                PGSDKLogger.error("\(__FUNCTION__): JSON Serialization error: \(error)")
+                
+                // And return a 500 (Internal Server Error) status code reponse.
+                self.respondWithErrorStatus(.InternalServerError, response, responseReturn, requestComplete)
+                return
+            }
+        }
+        else
+        {
+            // since identifierForVendor is an optional, we need to handle what happens if the value isn't returned.
+            // This would be very unusual, but again for this demo we're using full defensive coding practices.
+            
+            // Since the iOS environment should always have the identifierForVendor, it being nil is a odd error,
+            // one that warrents a generic 500 (Internal Server Error) status code reponse.
+            
+            self.respondWithErrorStatus(.InternalServerError, response, responseReturn, requestComplete)
+            return
+        }
+        
+    }
+
+    /* ****************************
+    The methods "registered" and "unregistered" are optional in the ServiceProtocol protocol. They are called when your service
+    is registered/unregistered in the ServiceRouter. The ServiceRouter controls which services are available in the system. 
+    When a service is registered it is then capable of receiving requests. When it is unregistered it will no longer receive requests.
+    While services themselves are stateless, there may be times when they utilize non-stateless components, or need to prepare 
+    something in the system environment prior to being used. These methods allow for that type of interaction.
+    For this example service we will not use them.
+    **************************** */
+    //static func registered(){}
+    //static func unregistered(){}
+}
+
---
+++ VendorIDServiceTests.swift
@@ -0,0 +1,180 @@
+//
+//  VendorIDServiceTests.swift
+//  PredixMobileReferenceApp
+//
+//  Created by Johns, Andy (GE Corporate) on 1/14/16.
+//  Copyright © 2016 GE. All rights reserved.
+//
+
+/* ****************************
+Unit tests are a key component in all software development. In a services-based architecture
+they are critical to ensure the services are working properly, and changes do not negatively impact
+consumers of the service. 
+
+Many times services may be entirely written using unit tests to exercise the code and do
+development debugging. The first time a webapp interacts with your service may be during
+integration testing.
+
+There are many ways to write unit tests, and many methodologies of testing. For this example,
+we'll focus on ensuring tests are both positive and negative, and demonstrate some of the
+helpers we've included in the example PredixMobileReferenceAppTests target.
+**************************** */
+
+import XCTest
+
+// We need to import our app. We use the @testable attribute in order to call internal methods, which can be helpful in testing some scenarios.
+// In this simple example, we don't need to, but it's a good practice to remember.
+@testable import PredixMobileReferenceApp
+
+// We also need to import the Predix Mobile SDK Framework.
+import PredixMobileSDK
+
+
+class VendorIDServiceTests: XCTestCase {
+
+    // setUp, tearDown, testExample, and testPerformanceExample are all included in the default XCode Unit Test template.
+    // They are left here unaltered just for orientation and reference.
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    // We'll start our VendorIDService tests here. Obviously we could have deleted the unused testExample, and testPerformance example.
+    
+    // The file ServiceProtocolTestHelper extends the XCTest class with some helpful methods for testing Predix Mobile serviceProtocol implementing classes.
+    
+    func testGetVendorId()
+    {
+        // first a happy-case test. We'll call the VendorIdService and get a return. Simple cases like this can use a lot of the defaults from ServiceProtocolTestHelper.
+        
+        // With this serviceTester overload we provide our service calls, the path to call, and the status code we expect back from the service.
+        // There are also optional blocks we can provide to allow for detailed examination of the NSURLResponse object, and any returned NSData.
+        
+        // we could just construct a hardcoded path "http://pmapi/vendorid" but if constants or configuration variables exist, it's better to use them.
+        // API_SCHEME is a constant "http"
+        // PredixMobilityConfiguration.API_HOST is the currently configured host, which defaults to "pmapi".
+        let path = "\(API_SCHEME)://\(PredixMobilityConfiguration.API_HOST)/vendorid"
+        
+        // As service calls are asyncronous this entire interaction is wrapped in an XCTest expectation, and the timeout for expectations is 20 seconds.
+        // You can create new expectations and fulfill them in the testResponse and/or testData blocks.
+        // let's create an expectation, that we'll fulfill in the data block when we examine the return data. This will ensure our call actually does return data.
+        let dataExpectation = self.expectationWithDescription("\(__FUNCTION__): testData closure called expectation.")
+
+        // serviceTester registers the service in the Service Router, executes the request, and examines the returned status code.
+        // If provided the testResponse and testData blocks are be called.
+        // If the service does not call requestComplete within 20 seconds, or the returned status code is not the expected status code the test will fail.
+        self.serviceTester(VendorIDService.self, path: path, expectedStatusCode: .OK, testResponse: nil, testData: { (data: NSData) -> Void in
+            
+            // let's de-serialize the data into our exepected dictionary.
+            //  NSJSONSerialization.JSONObjectWithData can throw so we wrap this in a do/try/catch
+            do
+            {
+                let deserializedObj = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))
+                
+                // so we have an object, but we need to ensure it's the type of object we expect. 
+                // We're expecting a dictionary with a key and value both a string, so we'll try an optional casting
+                if let dict = deserializedObj as? [String: String]
+                {
+                    // yes, it's the correct type.
+                    // now check that we have the expected key
+                    XCTAssertNotNil(dict["vendorId"], "key \"vendorId\" not found in return dictionary: \(dict)")
+                    
+                    // now check that the value associated with the key matches what we expect: for this simple example service, the device's vendor id.
+                    XCTAssertEqual(dict["vendorId"], UIDevice.currentDevice().identifierForVendor!.UUIDString)
+                    
+                    // Yes, the XCTAssertEqual renders the XCTAssertNotNil redundant, but in more complicated testing scenarios the messages associated
+                    // with a test failure may be critical to knowing where a test failed, and debugging.
+                }
+                else
+                {
+                    XCTAssertTrue(false, "deserializedObj was not the expected type: \(deserializedObj)")
+                }
+            }
+            catch let error
+            {
+                XCTAssertTrue(false, "JSON deserialization of the returned data failed: \(error)")
+            }
+            // fulfill our expectation.
+            dataExpectation.fulfill()
+        })
+    }
+    
+    // Tests our return status when a path contains more than we expect
+    func testBadURL_ExtraPathComponents()
+    {
+        // create our "bad" URL example, with additional path after the "vendorid" part. This should return a 400 (Bad Request) error
+        let path = "\(API_SCHEME)://\(PredixMobilityConfiguration.API_HOST)/vendorid/some/additional/path"
+        
+        // now we run the service tester. Since we're expecting an error, we don't need a testData block, and in this case 
+        // we don't need to examine anything extra in the testResponse block so we won't include them.
+        self.serviceTester(VendorIDService.self, path: path, expectedStatusCode: .BadRequest, testResponse: nil, testData: nil)
+    }
+    
+    // Tests our return status when a path contains more than we expect
+    func testBadURL_Querystring()
+    {
+        // create our "bad" URL example, with a unnecessary query string
+        let path = "\(API_SCHEME)://\(PredixMobilityConfiguration.API_HOST)/vendorid?query=string"
+        
+        // now we run the service tester. Since we're expecting an error, we don't need a testData block, and in this case
+        // we don't need to examine anything extra in the testResponse block so we won't include them.
+        self.serviceTester(VendorIDService.self, path: path, expectedStatusCode: .BadRequest, testResponse: nil, testData: nil)
+    }
+    
+    
+    // Obviously we could test a lot more conditions. A good set of unit tests should try for at least 80-90% code coverage.
+    // After all, the more code coverage you have, the better able you are to ensure everything is working as expected.
+
+    // But for this example, we'll end with this test that demonstrates how you can use the testResponse block to ensure any
+    // custom headers are returning as expected, and also uses a serviceTester overload that takes a NSURLRequest parameter
+    // instead of a simple path.
+
+    func testBadHTTPMethod()
+    {
+
+        // Our request url string
+        let path = "\(API_SCHEME)://\(PredixMobilityConfiguration.API_HOST)/vendorid"
+
+        // create a mutable request:
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        // change the HTTPMethod to POST
+        request.HTTPMethod = "POST"
+        
+        // now we run the service tester. Since we're expecting an error, and our service only returns data when there are no errors, we 
+        // don't need a testData block. But, we will include a testResponse block to ensure our headers are being added properly.
+        self.serviceTester(VendorIDService.self, request: request, expectedStatusCode: .MethodNotAllowed, testResponse: { (response: NSURLResponse) -> Void in
+            
+            // we need to cast the reponse. We could be more defensive here by optionally casting and doing an XCTAssert if it failed...
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            // now check that our expected "Allow" header is there, and is the value we expect.
+            XCTAssertEqual(httpResponse.allHeaderFields["Allow"] as? String, "GET", "Allow header was not as expected")
+            
+            }, testData: nil)
+
+    }
+    
+    
+    
+
+}
